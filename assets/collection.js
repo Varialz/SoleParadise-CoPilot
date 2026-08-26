d2f8334843a2b59ea3b9f9ff88e9fc779730f5b9
Varialz <81883266+Varialz@users.noreply.github.com>
2026-08-26 11:04:00 -0500

    chore: establish collection enhancement module

---
+++ assets/collection.js
@@ -0,0 +1,5 @@
+(() => {
+  'use strict';
+  // Reserved for the richer LacedUp-style filter enhancement.
+  // The current collection page remains fully functional through native GET sorting.
+})();
